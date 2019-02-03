0f58e35954cbc4ce2924e892142ff85130e23dc4
anosv <crazzyzyzy@rambler.ru>
2019-02-03 23:08:47 +0300

    add assertAll

--- src/test/java/ru/anosvqa/vae/rest_api_tests/GitHubTests.java
+++ src/test/java/ru/anosvqa/vae/rest_api_tests/GitHubTests.java
@@ -3,11 +3,14 @@ package ru.anosvqa.vae.rest_api_tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import ru.anosvqa.vae.rest_api_tests.desc.*;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * http://square.github.io/retrofit/
@@ -27,11 +30,16 @@ public class GitHubTests {
                 .build();
 
         GitHubService gitHub = retrofit.create(GitHubService.class);    //Создаем объект, при помощи которого будем выполнять запросы
-        List<Repository> repositories = gitHub.listRepos(USER_NAME).execute().body();
+        Response response = gitHub.listRepos(USER_NAME).execute();
+
+        List<Repository> repositories = (List<Repository>) response.body();
+
+        assertAll(
+                () -> assertEquals(response.code(), 200, "Код ответа доложен быть 200! Фактичеки: " + response.code()),
+                () -> assertTrue(repositories.size() > 0, "Кол-во репозиториев должно быть > 0")
+        );
 
         repositories.forEach( r -> System.out.println(r.getName()));
-
-        Assertions.assertTrue(repositories.size() > 0, "Кол-во репозиториев должно быть > 0");
     }
 
 }
